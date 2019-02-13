4e790682c96570d6a19ce1c5271a371eabc3d119
kaushalprateek <pkausha4@my.centennialcollege.ca>
2019-02-13 02:38:18 -0500

    mlab changes

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Home" });
 });
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+router.get("/about", function(req, res, next) {
+  res.render("index", { title: "About" });
 });
-router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contacts' });
+router.get("/contact", function(req, res, next) {
+  res.render("index", { title: "Contacts" });
 });
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products' });
+router.get("/products", function(req, res, next) {
+  res.render("index", { title: "Products" });
 });
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+router.get("/services", function(req, res, next) {
+  res.render("index", { title: "Services" });
 });
 module.exports = router;
